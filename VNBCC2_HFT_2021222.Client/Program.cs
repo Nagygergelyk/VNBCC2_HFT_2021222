2983423ebea07a7831aeaa1d2c9598fc0a4bff5d
gergely.kartal.nagy@stud.uni-obuda.hu <gergely.kartal.nagy@stud.uni-obuda.hu>
2022-04-02 14:46:42 +0200

    program.cs test added

--- VNBCC2_HFT_2021222.Client/Program.cs
+++ VNBCC2_HFT_2021222.Client/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using VNBCC2_HFT_2021222.Repository.Data;
 
 namespace VNBCC2_HFT_2021222.Client
 {
@@ -6,7 +7,13 @@ namespace VNBCC2_HFT_2021222.Client
     {
         static void Main(string[] args)
         {
+            GuitarShopDbContext ctx = new GuitarShopDbContext();
 
+            foreach (var item in ctx.Guitars)
+            {
+                Console.WriteLine(item.Brand + "; " + item.Shape + "; " + item.Year);
+            }
+            Console.ReadLine();
         }
     }
 }
