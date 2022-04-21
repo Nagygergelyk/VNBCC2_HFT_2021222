124bd84b6f73e348e99443dd159db2ff1c263b05
gergely.kartal.nagy@stud.uni-obuda.hu <gergely.kartal.nagy@stud.uni-obuda.hu>
2022-04-21 15:54:20 +0200

    All empty Logic Interfaces and classes added

---
+++ VNBCC2_HFT_2021222.Logic/Interfaces/IGuitarLogic.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VNBCC2_HFT_2021222.Logic.Interfaces
+{
+    public interface IGuitarLogic
+    {
+
+    }
+}
---
+++ VNBCC2_HFT_2021222.Logic/Interfaces/IShapeLogic.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VNBCC2_HFT_2021222.Logic.Interfaces
+{
+    interface IShapeLogic
+    {
+    }
+}
